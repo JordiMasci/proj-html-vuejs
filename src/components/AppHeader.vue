d4862ad0171097fb460bc039dc692cb0a751bda7
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-19 13:16:08 +0200

    feat: istall bootstrap/fontawesome..

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,50 @@
+<script>
+// import MyComponent from "./components/MyComponent. vue!";
+
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <header>
+    <!-- HEADER 1 -->
+    <div id="header-1">
+      <div class="container d-flex">
+        <div>
+          <span> info@example.com </span>
+          <span> +1(817)9013377 </span>
+        </div>
+        <div>
+            <font-awesome-icon :icon="['fab', 'facebook']" />
+        </div>
+    </div>
+    </div>
+    <!-- HEADER 2 -->
+    <div id="header-2">
+        <div class="container"></div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  #header-1 {
+    min-height: 50px;
+    background-color: #7f7045;
+  }
+
+  #header-2 {
+    min-height: 100px;
+    background-color: blue;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// Import Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap";
 
-createApp(App).mount('#app')
+// Import Fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+
+library.add(faFacebook, faTwitter, faInstagram, faLinkedin);
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
